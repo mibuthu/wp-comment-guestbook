94acd98faa5244347e9a443fa6b5dd718ccd1b20
Michael Burtscher <mike@mbnet.at>
2012-11-03 12:23:44 +0100

    removed not required lines

--- comment-guestbook.php
+++ comment-guestbook.php
@@ -38,8 +38,6 @@ class comment_guestbook {
 	public function __construct() {
 
 		// ALWAYS:
-		// TODO: replace "plugin-name-locale" with a unique value for your plugin
-		//load_plugin_textdomain( 'plugin-name-locale', false, dirname( plugin_basename( __FILE__ ) ) . '/lang' );
 		// Register shortcodes
 		add_shortcode( 'comment-guestbook', array( &$this, 'shortcode_comment_guestbook' ) );
 
@@ -49,59 +47,10 @@ class comment_guestbook {
 			require_once( 'php/admin.php' );
 			$admin = new cgb_admin();
 			// Register actions
-			//add_action( 'admin_print_styles', array( &$this, 'register_admin_styles' ) );
-			//add_action( 'admin_enqueue_scripts', array( &$this, 'register_admin_scripts' ) );
 			add_action( 'plugins_loaded', array( &$admin->options, 'version_upgrade' ) );
 			add_action( 'admin_menu', array( &$admin, 'register_pages' ) );
 		}
-		// FRONT PAGE:
-		//else {
-			// Register actions
-			//add_action( 'wp_enqueue_scripts', array( &$this, 'register_plugin_styles' ) );
-			//add_action( 'wp_enqueue_scripts', array( &$this, 'register_plugin_scripts' ) );
-		//}
 	} // end constructor
-
-	/**
-	 * Registers and enqueues admin-specific styles.
-	 */
-/*	public function register_admin_styles() {
-
-		// TODO change 'plugin-name' to the name of your plugin
-		wp_register_style( 'plugin-name-admin-styles', plugins_url( 'plugin-name/css/admin.css' ) );
-		wp_enqueue_style( 'plugin-name-admin-styles' );
-	} // end register_admin_styles
-*/
-	/**
-	 * Registers and enqueues admin-specific JavaScript.
-	 */
-/*	public function register_admin_scripts() {
-
-		// TODO change 'plugin-name' to the name of your plugin
-		wp_register_script( 'plugin-name-admin-script', plugins_url( 'plugin-name/js/admin.js' ) );
-		wp_enqueue_script( 'plugin-name-admin-script' );
-	} // end register_admin_scripts
-*/
-	/**
-	 * Registers and enqueues plugin-specific styles.
-	 */
-/*	public function register_plugin_styles() {
-
-		// TODO change 'plugin-name' to the name of your plugin
-		wp_register_style( 'plugin-name-plugin-styles', plugins_url( 'plugin-name/css/display.css' ) );
-		wp_enqueue_style( 'plugin-name-plugin-styles' );
-	} // end register_plugin_styles
-*/
-	/**
-	 * Registers and enqueues plugin-specific scripts.
-	 */
-/*	public function register_plugin_scripts() {
-
-		// TODO change 'plugin-name' to the name of your plugin
-		wp_register_script( 'plugin-name-plugin-script', plugins_url( 'plugin-name/js/display.js' ) );
-		wp_enqueue_script( 'plugin-name-plugin-script' );
-	} // end register_plugin_scripts
-*/
 
 	public function shortcode_comment_guestbook( $atts ) {
 		require_once( 'php/sc_comment-guestbook.php' );

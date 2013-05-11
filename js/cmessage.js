a20f1a96a6419e797901f130056d80f5695233f4
Michael Burtscher <mike@mbnet.at>
2013-05-11 16:29:01 +0200

    added styles an animation for inline message

--- js/cmessage.js
+++ js/cmessage.js
@@ -1,5 +1,7 @@
 (function($) {
 	// show the message after a comment was made
+	var message_style = 'background-color:rgb(255, 255, 224); border-color:rgb(230, 219, 85); color:rgb(51, 51, 51); padding:3px; margin:10px;'
+	                  + 'text-align:center; border-radius:3px 3px 3px 3px; border-width:1px; border-style:solid';
 	if ( window.cmessage_type == 'overlay' ) {
 		$.blockUI({
 			message: '<strong>'+window.cmessage_text+'</strong>',
@@ -10,6 +12,11 @@
 	}
 	else { // inline
 		var hash = window.location.hash;
-		$(hash).append('<div class="cmessage" style="color:rgb(120,220,20)"><strong>'+window.cmessage_text+'</strong></div>');
+		$('<div id="cmessage" style="'+message_style+'; display:none"><strong>'+window.cmessage_text+'</strong></div>')
+			.appendTo(hash)
+			.delay(500)
+			.slideDown()
+			.delay(3000)
+			.slideUp();
 	}
 })(jQuery);

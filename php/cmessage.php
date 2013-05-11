5e043bbaa303a8551ab059149fee636f4e44a951
Michael Burtscher <mike@mbnet.at>
2013-05-11 13:15:29 +0200

    fixed cmessage file

--- php/cmessage.php
+++ php/cmessage.php
@@ -19,28 +19,16 @@ class CGB_CMessage {
 	}
 
 	public function init() {
-<<<<<<< HEAD
 		add_action( 'init', array( &$this, 'register_scripts' ) );
 		add_action( 'wp_footer', array( &$this, 'print_scripts' ) );
 	}
 
 	public function register_scripts() {
-=======
-		add_action( 'init', array( &$this, 'frontpage_init' ) );
-		add_action( 'wp_footer', array( &$this, 'frontpage_footer' ) );
-	}
-
-	public function frontpage_init() {
->>>>>>> 9a6ca791033c4b662e0b46205d20362a2c6e7ce9
 		wp_register_script( 'block_ui', 'http://malsup.github.com/jquery.blockUI.js', array( 'jquery' ), true );
 		wp_register_script( 'cgb_comment_guestbook', CGB_URL.'js/comment-guestbook.js', array( 'block_ui' ), true );
 	}
 
-<<<<<<< HEAD
 	public function print_scripts() {
-=======
-	public function frontpage_footer() {
->>>>>>> 9a6ca791033c4b662e0b46205d20362a2c6e7ce9
 		$this->print_script_variables();
 		wp_print_scripts( 'cgb_comment_guestbook' );
 	}
@@ -55,11 +43,7 @@ class CGB_CMessage {
 		return $url;
 	}
 
-<<<<<<< HEAD
 	private function print_script_variables() {
-=======
-	public function print_script_variables() {
->>>>>>> 9a6ca791033c4b662e0b46205d20362a2c6e7ce9
 		$out = '
 			<script type="text/javascript">
 				var cmessage_text = "'.$this->options->get( 'cgb_cmessage_text' ).'";

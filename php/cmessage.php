4488452af1827ed4acc76f4af4d25e864c73f3c6
Michael Burtscher <mike@mbnet.at>
2013-05-12 10:21:43 +0200

    removed space after opening and before closing bracket in cmessage class

--- php/cmessage.php
+++ php/cmessage.php
@@ -1,5 +1,5 @@
 <?php
-if( !defined( 'ABSPATH' ) ) {
+if(!defined('ABSPATH')) {
 	exit;
 }
 
@@ -11,7 +11,7 @@ class CGB_CMessage {
 	private $options;
 
 	public static function &get_instance() {
-		if( !isset( self::$instance ) ) {
+		if(!isset(self::$instance)) {
 			self::$instance = new CGB_CMessage();
 		}
 		return self::$instance;
@@ -22,24 +22,24 @@ class CGB_CMessage {
 	}
 
 	public function init() {
-		add_action( 'init', array( &$this, 'register_scripts' ) );
-		add_action( 'wp_footer', array( &$this, 'print_scripts' ) );
+		add_action('init', array(&$this, 'register_scripts'));
+		add_action('wp_footer', array(&$this, 'print_scripts'));
 	}
 
 	public function register_scripts() {
-		wp_register_script( 'cgb_cmessage', CGB_URL.'js/cmessage.js', array( 'jquery' ), true );
+		wp_register_script('cgb_cmessage', CGB_URL.'js/cmessage.js', array('jquery'), true);
 	}
 
 	public function print_scripts() {
 		$this->print_script_variables();
-		wp_print_scripts( 'cgb_cmessage' );
+		wp_print_scripts('cgb_cmessage');
 	}
 
-	public function add_cmessage_indicator( $url ) {
-		if( 'always' === $this->options->get( 'cgb_cmessage' ) ||
-				( 'guestbook_only' === $this->options->get( 'cgb_cmessage' ) && isset( $_POST['is_cgb_comment'] ) ) ) {
-			$url_array = explode( '#', $url );
-			$query_delimiter = ( false !== strpos( $url_array[0], '?' ) ) ? '&' : '?';
+	public function add_cmessage_indicator($url) {
+		if('always' === $this->options->get('cgb_cmessage') ||
+				('guestbook_only' === $this->options->get('cgb_cmessage') && isset($_POST['is_cgb_comment']))) {
+			$url_array = explode('#', $url);
+			$query_delimiter = (false !== strpos($url_array[0], '?')) ? '&' : '?';
 			$url = $url_array[0].$query_delimiter.'cmessage=1#'.$url_array[1];
 		}
 		return $url;
@@ -48,8 +48,8 @@ class CGB_CMessage {
 	private function print_script_variables() {
 		$out = '
 			<script type="text/javascript">
-				var cmessage_text = "'.$this->options->get( 'cgb_cmessage_text' ).'";
-				var cmessage_type = "'.$this->options->get( 'cgb_cmessage_type' ).'";
+				var cmessage_text = "'.$this->options->get('cgb_cmessage_text').'";
+				var cmessage_type = "'.$this->options->get('cgb_cmessage_type').'";
 			</script>';
 		echo $out;
 	}

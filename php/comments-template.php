64c71fd25385a8403e8dd9c77b9a4415d3a5fa30
Michael Burtscher <mike@mbnet.at>
2013-08-17 12:39:50 +0200

    changed formatting

--- php/comments-template.php
+++ php/comments-template.php
@@ -2,17 +2,17 @@
 /**
  * The custom template for displaying Comments for comment-guestbook plugin.
  */
-if( !defined( 'ABSPATH' ) ) {
+if(!defined('ABSPATH')) {
 	exit;
 }
 
-require_once( CGB_PATH.'php/comments-functions.php' );
+require_once(CGB_PATH.'php/comments-functions.php');
 $cgb_func = new cgb_comments_functions();
 ?>
 	<div id="comments">
-	<?php $cgb_func->show_comment_form_html( 'above_comments' ) ?>
-	<?php if ( post_password_required() ) : ?>
-		<p class="nopassword"><?php _e( 'This post is password protected. Enter the password to view any comments.', $cgb_func->l10n_domain ); ?></p>
+	<?php $cgb_func->show_comment_form_html('above_comments') ?>
+	<?php if(post_password_required()) : ?>
+		<p class="nopassword"><?php _e('This post is password protected. Enter the password to view any comments.', $cgb_func->l10n_domain); ?></p>
 	<?php
 			/* Stop the rest of comments.php from being processed,
 			 * but don't kill the script entirely -- we still have
@@ -23,19 +23,19 @@ $cgb_func = new cgb_comments_functions();
 		endif;
 	?>
 
-	<?php if( have_comments() ) : ?>
-   <?php /*
+	<?php if(have_comments()) : ?>
+		<?php /* TODO: Insert an option to add a title before the comment list
 		<h2 id="comments-title">
 			<?php
 				printf( get_the_title().' Entries:' );
 			?>
 		</h2>
 		*/
-	?>
-	<?php if( get_comment_pages_count() > 1 && get_option( 'page_comments' ) ) : // are there comments to navigate through ?>
-		<nav id="comment-nav-above">
-			<?php $cgb_func->show_nav_html(); ?>
-		</nav>
+		?>
+		<?php if(get_comment_pages_count() > 1 && get_option('page_comments')) : // are there comments to navigate through ?>
+			<nav id="comment-nav-above">
+				<?php $cgb_func->show_nav_html(); ?>
+			</nav>
 		<?php endif; // check for comment navigation ?>
 
 		<ol class="commentlist">
@@ -47,11 +47,11 @@ $cgb_func = new cgb_comments_functions();
 			?>
 		</ol>
 
-		<?php if( get_comment_pages_count() > 1 && get_option( 'page_comments' ) ) : // are there comments to navigate through ?>
+		<?php if(get_comment_pages_count() > 1 && get_option('page_comments')) : // are there comments to navigate through ?>
 		<nav id="comment-nav-below">
 			<?php $cgb_func->show_nav_html(); ?>
 		</nav>
 		<?php endif; // check for comment navigation ?>
 	<?php endif; ?>
-	<?php $cgb_func->show_comment_form_html( 'below_comments' ) ?>
+	<?php $cgb_func->show_comment_form_html('below_comments') ?>
 	</div><!-- #comments -->

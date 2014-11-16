0b9b7a9985797c3b40dd2c4c612822f77551d698
Michael Burtscher <mike@mbnet.at>
2014-11-16 15:53:56 +0100

    include auto feature for truncate
    With the new option "auto" for the truncate length the truncate will be done automatically
    via css.

--- includes/widget.php
+++ includes/widget.php
@@ -80,7 +80,7 @@ class CGB_Widget extends WP_Widget {
 			                                'std_value'     => '18',
 			                                'caption'       => __('Truncate author to'),
 			                                'caption_after' => __('characters'),
-			                                'tooltip'       => __('If the comment author is displayed this option limits the number of displayed characters. Set this value to 0 to view the full author.'),
+			                                'tooltip'       => __('If the comment author is displayed this option limits the number of displayed characters. Set this value to "0" to view the full author or "auto" to automatically truncate the text via css.'),
 			                                'form_style'    => 'margin:0 0 0.6em 0.9em',
 			                                'form_width'    => 30),
 
@@ -96,7 +96,7 @@ class CGB_Widget extends WP_Widget {
 			                                'std_value'     => '18',
 			                                'caption'       => __('Truncate title to'),
 			                                'caption_after' => __('characters'),
-			                                'tooltip'       => __('If the comment page title is displayed this option limits the number of displayed characters. Set this value to 0 to view the full title.'),
+			                                'tooltip'       => __('If the comment page title is displayed this option limits the number of displayed characters. Set this value to 0 to view the full title or "auto" to automatically truncate the text via css..'),
 			                                'form_style'    => 'margin:0 0 0.6em 0.9em',
 			                                'form_width'    => 30),
 
@@ -112,7 +112,7 @@ class CGB_Widget extends WP_Widget {
 			                                'std_value'     => '25',
 			                                'caption'       => __('Truncate text to '),
 			                                'caption_after' => __('characters'),
-			                                'tooltip'       => __('If the comment text is displayed this option limits the number of displayed characters. Set this value to 0 to view the full text.'),
+			                                'tooltip'       => __('If the comment text is displayed this option limits the number of displayed characters. Set this value to 0 to view the full text or "auto" to automatically truncate the text via css..'),
 			                                'form_style'    => 'margin:0 0 0.6em 0.9em',
 			                                'form_width'    => 30),
 
@@ -215,7 +215,7 @@ class CGB_Widget extends WP_Widget {
 					$out .= '<span class="cgb-date" title="'.__('Date of comment:').' '.esc_attr(get_comment_date()).'">'.get_comment_date($instance['date_format']).' </span>';
 				}
 				if('true' === $instance['show_author']) {
-					$out .= '<span class="cgb-author" title="'.__('Comment author:').' '.esc_attr(get_comment_author()).'">'.$this->truncate($instance['author_length'], get_comment_author()).'</span>';
+					$out .= $this->truncate($instance['author_length'], get_comment_author(), 'span', array('class' => 'cgb-author', 'title' => __('Comment author:').' '.esc_attr(get_comment_author())));
 				}
 				if('true' === $instance['show_page_title']) {
 					if('false' === $instance['hide_gb_page_title'] || url_to_postid($instance['url_to_page']) != $comment->comment_post_ID) {
@@ -230,7 +230,7 @@ class CGB_Widget extends WP_Widget {
 					$out .= '</a>';
 				}
 				if('true' === $instance['show_comment_text']) {
-					$out .= '<div class="cgb-widget-text" title="'.esc_attr(get_comment_text()).'">'.$this->truncate($instance['comment_text_length'], get_comment_text()).'</div>';
+					$out .= $this->truncate($instance['comment_text_length'], get_comment_text(), 'div', array('class' => 'cgb-widget-text', 'title' => esc_attr(get_comment_text())));
 				}
 				$out .= '</li>';
 			}
@@ -353,9 +353,38 @@ class CGB_Widget extends WP_Widget {
 	 *
 	 * @param int $max_length The length to which the text should be shortened
 	 * @param string $html The html code which should be shortened
+	 * @param string $wrapper_type Defines which kind of wrapper shall be added
+	 *               around the html code if a manual length shall be used.
+	 *               The possible values are "none", "div" and "span".
+	 *               With "none" no wrapper will be added, with "div" and "span"
+	 *               you can define the 2 available wrapper type.
+	 *               If max_length is set to auto a div is mandatory and
+	 *               will be added always.
+	 * @param array $wrapper_div_attributes Additional attributes for the
+	 *              wrapper element. Use the attribute name as the array key.
 	 ***************************************************************************/
-	private function truncate($max_length, $html) {
-		if($max_length > 0 && mb_strlen($html) > $max_length) {
+	private function truncate($max_length, $html, $wrapper_type='none', $wrapper_attributes=array()) {
+		// Apply wrapper and add required css for autolength (if required)
+		$autolength = 'auto' == $max_length ? true : false;
+		if($autolength) {
+			$wrapper_type = 'div';
+		}
+		elseif('div' != $wrapper_type && 'span' != $wrapper_type) {
+			$wrapper_type = 'none';
+		}
+		if('none' != $wrapper_type) {
+			$wrapper_text = '<'.$wrapper_type;
+			foreach($wrapper_attributes as $name => $value) {
+				$wrapper_text .= ' '.$name.'="'.$value.'"';
+			}
+			if($autolength) {
+				$wrapper_text .= ' style="white-space:nowrap; overflow:hidden; text-overflow:ellipsis"';
+			}
+			$html = $wrapper_text.'>'.$html.'</'.$wrapper_type.'>';
+		}
+
+		// Apply manual length
+		if(is_numeric($max_length) && 0 < $max_length && mb_strlen($html) > $max_length) {
 			$printedLength = 0;
 			$position = 0;
 			$tags = array();
@@ -413,9 +442,7 @@ class CGB_Widget extends WP_Widget {
 			}
 			return $out;
 		}
-		else {
-			return $html;
-		}
+		return $html;
 	}
 }
 ?>

95060b8bb0860685dd130664e6faf28a610ed27a
mibuthu <mike@mbnet.at>
2018-12-08 16:21:32 +0100

    refactor includes/options.php and includes/options-helptexts.php
    * switch to WordPress coding standard
    * fixed/added doc blocks
    * use php strict_types mode
    * updated code comments
    * renamed file options-helptexts.php
    * moved section from options.php to options-helptexts.php
    * updated / prepare all helptexts for translations

--- includes/options.php
+++ includes/options.php
@@ -1,124 +1,177 @@
 <?php
-if(!defined('WPINC')) {
-	exit;
+/**
+ * Options Class
+ *
+ * @package comment-guestbook
+ */
+
+declare(strict_types=1);
+if ( ! defined( 'WPINC' ) ) {
+	die;
 }
 
-// This class handles all available options
+/**
+ * Options class
+ *
+ * This class handles all available options with their information
+ */
 class CGB_Options {
 
+	/**
+	 * Class singleton instance reference
+	 *
+	 * @var object
+	 */
 	private static $instance;
+
+	/**
+	 * Option sections
+	 *
+	 * @var array
+	 */
 	public $sections;
+
+	/**
+	 * Options array
+	 *
+	 * @var array
+	 */
 	public $options;
 
+
+	/**
+	 * Singleton provider and setup
+	 *
+	 * @return object
+	 */
 	public static function &get_instance() {
-		// Create class instance if required
-		if(!isset(self::$instance)) {
+		if ( ! isset( self::$instance ) ) {
 			self::$instance = new self();
 		}
-		// Return class instance
 		return self::$instance;
 	}
 
+
+	/**
+	 * Class constructor which initializes required variables
+	 *
+	 * @return void
+	 */
 	private function __construct() {
-		add_action('init', array(&$this, 'init_options'), 1);
-		add_action('admin_init', array(&$this, 'register_options'));
+		add_action( 'init', array( &$this, 'init' ), 1 );
+		add_action( 'admin_init', array( &$this, 'register' ) );
 	}
 
-	public function init_options() {
+
+	/**
+	 * Initialize the options
+	 *
+	 * @return void
+	 */
+	public function init() {
 		$this->options = array(
-			'cgb_ignore_comments_open'        => array('section' => 'general',       'std_val' => '1'),
-			'cgb_ignore_comment_registration' => array('section' => 'general',       'std_val' => '1'),
-			'cgb_ignore_comment_moderation'   => array('section' => 'general',       'std_val' => ''),
-			'cgb_threaded_gb_comments'        => array('section' => 'general',       'std_val' => 'default'),
-			'cgb_adjust_output'               => array('section' => 'general',       'std_val' => ''),
-			'cgb_l10n_domain'                 => array('section' => 'general',       'std_val' => 'default'),
-
-			'cgb_form_below_comments'         => array('section' => 'comment_form',  'std_val' => ''),
-			'cgb_form_above_comments'         => array('section' => 'comment_form',  'std_val' => ''),
-			'cgb_form_in_page'                => array('section' => 'comment_form',  'std_val' => '1'),
-			'cgb_form_expand_type'            => array('section' => 'comment_form',  'std_val' => 'false'),
-			'cgb_form_expand_link_text'       => array('section' => 'comment_form',  'std_val' => __('Add a new guestbook entry','comment-guestbook')),
-			'cgb_add_cmessage'                => array('section' => 'comment_form',  'std_val' => ''),
-			'cgb_form_require_no_name_mail'   => array('section' => 'comment_form',  'std_val' => ''),
-			'cgb_form_remove_mail'            => array('section' => 'comment_form',  'std_val' => ''),
-			'cgb_form_remove_website'         => array('section' => 'comment_form',  'std_val' => ''),
-			'cgb_form_comment_label'          => array('section' => 'comment_form',  'std_val' => 'default'),
-			'cgb_form_title_reply'            => array('section' => 'comment_form',  'std_val' => 'default'),
-			'cgb_form_title_reply_to'         => array('section' => 'comment_form',  'std_val' => 'default'),
-			'cgb_form_notes_before'           => array('section' => 'comment_form',  'std_val' => 'default'),
-			'cgb_form_notes_after'            => array('section' => 'comment_form',  'std_val' => 'default'),
-			'cgb_form_label_submit'           => array('section' => 'comment_form',  'std_val' => 'default'),
-			'cgb_form_cancel_reply'           => array('section' => 'comment_form',  'std_val' => 'default'),
-			'cgb_form_must_login_message'     => array('section' => 'comment_form',  'std_val' => 'default'),
-			'cgb_form_styles'                 => array('section' => 'comment_form',  'std_val' => ''),
-			'cgb_form_args'                   => array('section' => 'comment_form',  'std_val' => ''),
-
-			'cgb_clist_order'                 => array('section' => 'comment_list',  'std_val' => 'default'),
-			'cgb_clist_child_order'           => array('section' => 'comment_list',  'std_val' => 'default'),
-			'cgb_clist_default_page'          => array('section' => 'comment_list',  'std_val' => 'default'),
-			'cgb_clist_pagination'            => array('section' => 'comment_list',  'std_val' => 'default'),
-			'cgb_clist_per_page'              => array('section' => 'comment_list',  'std_val' => '0'),
-			'cgb_clist_show_all'              => array('section' => 'comment_list',  'std_val' => ''),
-			'cgb_clist_num_pagination'        => array('section' => 'comment_list',  'std_val' => ''),
-			'cgb_clist_title'                 => array('section' => 'comment_list',  'std_val' => ''),
-			'cgb_clist_in_page_content'       => array('section' => 'comment_list',  'std_val' => ''),
-			'cgb_comment_callback'            => array('section' => 'comment_list',  'std_val' => '--func--comment_callback'),
-			'cgb_clist_styles'                => array('section' => 'comment_list',  'std_val' => ''),
-			'cgb_clist_args'                  => array('section' => 'comment_list',  'std_val' => ''),
-
-			'cgb_comment_adjust'              => array('section' => 'comment_html',  'std_val' => ''),
-			'cgb_comment_html'                => array('section' => 'comment_html',  'std_val' => '--func--comment_html'),
-
-			'cgb_cmessage_text'               => array('section' => 'cmessage',      'std_val' => __('Thanks for your comment','comment-guestbook')),
-			'cgb_cmessage_type'               => array('section' => 'cmessage',      'std_val' => 'inline'),
-			'cgb_cmessage_duration'           => array('section' => 'cmessage',      'std_val' => '3000'),
-			'cgb_cmessage_styles'             => array('section' => 'cmessage',      'std_val' => 'background-color:rgb(255, 255, 224);&#10;border-color:rgb(230, 219, 85);&#10;color:rgb(51, 51, 51);&#10;padding:6px 20px;&#10;text-align:center;&#10;border-radius:5px;&#10;border-width:1px;&#10;border-style:solid'),
-
-			'cgb_page_add_cmessage'           => array('section' => 'page_comments', 'std_val' => ''),
-			'cgb_page_remove_mail'            => array('section' => 'page_comments', 'std_val' => ''),
-			'cgb_page_remove_website'         => array('section' => 'page_comments', 'std_val' => ''),
+			// General.
+			'cgb_ignore_comments_open'        => array( 'std_val' => '1' ),
+			'cgb_ignore_comment_registration' => array( 'std_val' => '1' ),
+			'cgb_ignore_comment_moderation'   => array( 'std_val' => '' ),
+			'cgb_threaded_gb_comments'        => array( 'std_val' => 'default' ),
+			'cgb_adjust_output'               => array( 'std_val' => '' ),
+			'cgb_l10n_domain'                 => array( 'std_val' => 'default' ),
+			// Comment form.
+			'cgb_form_below_comments'         => array( 'std_val' => '' ),
+			'cgb_form_above_comments'         => array( 'std_val' => '' ),
+			'cgb_form_in_page'                => array( 'std_val' => '1' ),
+			'cgb_form_expand_type'            => array( 'std_val' => 'false' ),
+			'cgb_form_expand_link_text'       => array( 'std_val' => __( 'Add a new guestbook entry', 'comment-guestbook' ) ),
+			'cgb_add_cmessage'                => array( 'std_val' => '' ),
+			'cgb_form_require_no_name_mail'   => array( 'std_val' => '' ),
+			'cgb_form_remove_mail'            => array( 'std_val' => '' ),
+			'cgb_form_remove_website'         => array( 'std_val' => '' ),
+			'cgb_form_comment_label'          => array( 'std_val' => 'default' ),
+			'cgb_form_title_reply'            => array( 'std_val' => 'default' ),
+			'cgb_form_title_reply_to'         => array( 'std_val' => 'default' ),
+			'cgb_form_notes_before'           => array( 'std_val' => 'default' ),
+			'cgb_form_notes_after'            => array( 'std_val' => 'default' ),
+			'cgb_form_label_submit'           => array( 'std_val' => 'default' ),
+			'cgb_form_cancel_reply'           => array( 'std_val' => 'default' ),
+			'cgb_form_must_login_message'     => array( 'std_val' => 'default' ),
+			'cgb_form_styles'                 => array( 'std_val' => '' ),
+			'cgb_form_args'                   => array( 'std_val' => '' ),
+			// Comment list.
+			'cgb_clist_order'                 => array( 'std_val' => 'default' ),
+			'cgb_clist_child_order'           => array( 'std_val' => 'default' ),
+			'cgb_clist_default_page'          => array( 'std_val' => 'default' ),
+			'cgb_clist_pagination'            => array( 'std_val' => 'default' ),
+			'cgb_clist_per_page'              => array( 'std_val' => '0' ),
+			'cgb_clist_show_all'              => array( 'std_val' => '' ),
+			'cgb_clist_num_pagination'        => array( 'std_val' => '' ),
+			'cgb_clist_title'                 => array( 'std_val' => '' ),
+			'cgb_clist_in_page_content'       => array( 'std_val' => '' ),
+			'cgb_comment_callback'            => array( 'std_val' => '--func--comment_callback' ),
+			'cgb_clist_styles'                => array( 'std_val' => '' ),
+			'cgb_clist_args'                  => array( 'std_val' => '' ),
+			// Comment html code.
+			'cgb_comment_adjust'              => array( 'std_val' => '' ),
+			'cgb_comment_html'                => array( 'std_val' => '--func--comment_html' ),
+			// Message after new comment.
+			'cgb_cmessage_text'               => array( 'std_val' => __( 'Thanks for your comment', 'comment-guestbook' ) ),
+			'cgb_cmessage_type'               => array( 'std_val' => 'inline' ),
+			'cgb_cmessage_duration'           => array( 'std_val' => '3000' ),
+			'cgb_cmessage_styles'             => array( 'std_val' => 'background-color:rgb(255, 255, 224);&#10;border-color:rgb(230, 219, 85);&#10;color:rgb(51, 51, 51);&#10;padding:6px 20px;&#10;text-align:center;&#10;border-radius:5px;&#10;border-width:1px;&#10;border-style:solid' ),
+			'cgb_page_add_cmessage'           => array( 'std_val' => '' ),
+			'cgb_page_remove_mail'            => array( 'std_val' => '' ),
+			'cgb_page_remove_website'         => array( 'std_val' => '' ),
 		);
 	}
 
+
+	/**
+	 * Register the options
+	 *
+	 * @return void
+	 */
+	public function register() {
+		foreach ( $this->options as $oname => $o ) {
+			register_setting( 'cgb_options', $oname );
+		}
+	}
+
+
+	/**
+	 * Load options helptexts from additional file
+	 *
+	 * @return void
+	 */
 	public function load_options_helptexts() {
-		require_once(CGB_PATH.'includes/options_helptexts.php');
-		foreach($options_helptexts as $name => $values) {
-			$this->options[$name] += $values;
+		require_once CGB_PATH . 'includes/options-helptexts.php';
+		foreach ( $cgb_options_helptexts as $name => $values ) {
+			$this->options[ $name ] += $values;
 		}
-		unset($options_helptexts);
+		unset( $cgb_options_helptexts );
 
-		$this->sections = $sections_helptexts;
-		unset($sections_helptexts);
+		$this->sections = $cgb_sections_helptexts;
+		unset( $cgb_sections_helptexts );
 	}
 
-	public function register_options() {
-		foreach($this->options as $oname => $o) {
-			register_setting('cgb_'.$o['section'], $oname);
-		}
-	}
 
-/*
-	public function set($name, $value) {
-		if(isset($this->options[$name])) {
-			return update_option($name, $value);
-		}
-		else {
-			return false;
-		}
-	}
-*/
-	public function get($name) {
-		if(isset($this->options[$name])) {
-			// set std_val, if a function is used to set the value
-			if(substr($this->options[$name]['std_val'], 0, 8) == '--func--') {
-				$this->options[$name]['std_val'] = call_user_func(array('cgb_options', substr($this->options[$name]['std_val'], 8)));
+	/**
+	 * Get the value of the specified option
+	 *
+	 * @param string $name Option name.
+	 * @return string Option value.
+	 */
+	public function get( $name ) {
+		if ( isset( $this->options[ $name ] ) ) {
+			// Set std_val, if a function is used to set the value.
+			if ( '--func--' === substr( $this->options[ $name ]['std_val'], 0, 8 ) ) {
+				$this->options[ $name ]['std_val'] = call_user_func( array( 'cgb_options', substr( $this->options[ $name ]['std_val'], 8 ) ) );
 			}
-			return get_option($name, $this->options[$name]['std_val']);
-		}
-		else {
+			return get_option( $name, $this->options[ $name ]['std_val'] );
+		} else {
 			return null;
 		}
 	}
+
 
 	/**
 	 * Upgrades renamed or modified options to the actual version
@@ -128,36 +181,47 @@ class CGB_Options {
 	 *   cgb_cmessage -> splitted up in cgb_add_cmessage and cgb_page_add_cmessage
 	 */
 	public function version_upgrade() {
-		$value = get_option('cgb_clist_adjust', null);
-		if(null != $value) {
-			add_option('cgb_adjust_output', $value);
-			delete_option('cgb_clist_adjust');
+		$value = get_option( 'cgb_clist_adjust', null );
+		if ( null !== $value ) {
+			add_option( 'cgb_adjust_output', $value );
+			delete_option( 'cgb_clist_adjust' );
 		}
-		$value = get_option('cgb_cmessage', null);
-		if(null != $value) {
-			if('default' != $value) {
-				add_option('cgb_add_cmessage', '1');
+		$value = get_option( 'cgb_cmessage', null );
+		if ( null !== $value ) {
+			if ( 'default' !== $value ) {
+				add_option( 'cgb_add_cmessage', '1' );
 			}
-			if('always' == $value) {
-				add_option('cgb_page_add_cmessage', '1');
+			if ( 'always' === $value ) {
+				add_option( 'cgb_page_add_cmessage', '1' );
 			}
-			delete_option('cgb_cmessage');
+			delete_option( 'cgb_cmessage' );
 		}
 	}
 
+
+	/**
+	 * Returns the default value for the comment_callback option
+	 *
+	 * @return string
+	 */
 	private function comment_callback() {
-		$func = get_stylesheet().'_comment';
-		if(function_exists($func)) {
+		$func = get_stylesheet() . '_comment';
+		if ( function_exists( $func ) ) {
 			return $func;
-		}
-		else {
+		} else {
 			return '';
 		}
 	}
 
+
+	/**
+	 * Returns the default value for the comment_html option
+	 *
+	 * @return string
+	 */
 	private function comment_html() {
-		// use 2 spaces instead of 1 tab to have a better view in the options dialog
-		$out = '<footer class="comment-meta">
+		return '
+<footer class="comment-meta">
 <div class="comment-author vcard">
 <?php
 	$avatar_size = 68;
@@ -182,6 +246,6 @@ class CGB_Options {
 <div class="reply">
 	<?php comment_reply_link(array_merge($args, array("reply_text" => __("Reply <span>&darr;</span>", $l10n_domain), "depth" => $depth, "max_depth" => $args["max_depth"]))); ?>
 </div><!-- .reply -->';
-		return $out;
 	}
+
 }
